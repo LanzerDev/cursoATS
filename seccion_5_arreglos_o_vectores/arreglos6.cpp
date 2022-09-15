8f42e89a278742a71d24bf352a8fa83a16935e1d
LanzerDev <78884322+LanzerDev@users.noreply.github.com>
2022-09-15 17:45:13 -0600

    arreglos6

--- seccion_5_arreglos_o_vectores/arreglos6.cpp
+++ seccion_5_arreglos_o_vectores/arreglos6.cpp
@@ -4,13 +4,21 @@ using namespace std;
 
 int main(){
     
-    int arr[5] = {5,2,3};
-
-    for (int i = 0; i < 3; i++)
+    int arr[5] = {5,2,3,10};
+    int may = 0;
+    int suma = 0;
+    for (int i = 0; i < 4; i++)
     {
-        if(arr[i] == arr[i+1] + arr[i+2]){
-            cout<<"El array tiene un numero que equivale a la suma del resto del array";
+        suma += arr[i];
+
+        if(arr[i] > may){
+            may = arr[i];
         }
+    }
+    if(may == suma - may){
+        cout<<"El numero: "<<may<<" es igual a la suma del resto de los elementos del array"<<endl;
+    } else {
+        cout<<"No hay ningun numero que sea la suma de los otros numeros del array";
     }
     
 
